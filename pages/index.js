366e4cf417c98338ad70b9c61df6c3a2b96a64ab
Ben Selby <benmatselby@gmail.com>
2021-06-20 22:11:47 +0100

    Add some ally fixes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
+import Head from "next/head";
 
 export default function Home() {
   const {
@@ -17,53 +18,62 @@ export default function Home() {
   console.log(watch("example")); // you can watch individual input by pass the name of the input
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Name</label>
-      <input
-        {...register("name", {
-          required: true,
-        })}
-      />
-      {errors?.name?.type === "required" && <p className="error">Please provide your name.</p>}
+    <>
+      <Head>
+        <title>Upload form</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          {...register("name", {
+            required: true,
+          })}
+        />
+        {errors?.name?.type === "required" && <p className="error">Please provide your name.</p>}
 
-      <label>Email</label>
-      <input
-        type="text"
-        {...register("email", {
-          required: true,
-          pattern:
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        })}
-      />
-      {errors?.email?.type === "required" && <p className="error">Please provide your email address.</p>}
-      {errors?.email?.type === "pattern" && <p className="error">Please provide a valid email address.</p>}
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="text"
+          {...register("email", {
+            required: true,
+            pattern:
+              /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+        />
+        {errors?.email?.type === "required" && <p className="error">Please provide your email address.</p>}
+        {errors?.email?.type === "pattern" && <p className="error">Please provide a valid email address.</p>}
 
-      <label>Organisation ID</label>
-      <input
-        {...register("org", {
-          required: true,
-          minLength: 3,
-          maxLength: 3,
-        })}
-      />
-      {errors?.org && <p className="error">You Organisation ID is 3 characters</p>}
+        <label htmlFor="org">Organisation ID</label>
+        <input
+          id="org"
+          {...register("org", {
+            required: true,
+            minLength: 3,
+            maxLength: 3,
+          })}
+        />
+        {errors?.org && <p className="error">You Organisation ID is 3 characters</p>}
 
-      <label>Comments</label>
-      <textarea rows="3" {...register("comments")} />
-      {errors.age && <p className="error">You Must be older then 18 and younger then 99 years old</p>}
+        <label htmlFor="comments">Comments</label>
+        <textarea id="comments" rows="3" {...register("comments")} />
+        {errors.age && <p className="error">You Must be older then 18 and younger then 99 years old</p>}
 
-      <label>Reference number</label>
-      <input {...register("ref", { minLength: 4, max: 4 })} />
-      {errors.ref && <p className="error">You Organisation ID is 3 characters</p>}
+        <label htmlFor="ref">Reference number</label>
+        <input id="ref" {...register("ref", { minLength: 4, max: 4 })} />
+        {errors.ref && <p className="error">You Organisation ID is 3 characters</p>}
 
-      <label>Add file</label>
-      <input type="file" {...register("file")} />
+        <label htmlFor="file">Add file</label>
+        <input id="file" type="file" {...register("file")} />
 
-      <label>Agree to the terms and conditions</label>
-      <input type="checkbox" {...register("terms", { required: true })} />
-      {errors?.terms && <p className="error">Please agree to the terms and conditions</p>}
+        <label htmlFor="terms">Agree to the terms and conditions</label>
+        <input id="terms" type="checkbox" {...register("terms", { required: true })} />
+        {errors?.terms && <p className="error">Please agree to the terms and conditions</p>}
 
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+      </form>
+    </>
   );
 }
