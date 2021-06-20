73303caeba2bc6924fc5be1a9255ee37d7294942
Ben Selby <benmatselby@gmail.com>
2021-06-20 22:03:23 +0100

    Some minor CSS fixes.

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
           required: true,
         })}
       />
-      {errors?.name?.type === "required" && <p>Please provide your name.</p>}
+      {errors?.name?.type === "required" && <p className="error">Please provide your name.</p>}
 
       <label>Email</label>
       <input
@@ -35,8 +35,8 @@ export default function Home() {
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         })}
       />
-      {errors?.email?.type === "required" && <p>Please provide your email address.</p>}
-      {errors?.email?.type === "pattern" && <p>Please provide a valid email address.</p>}
+      {errors?.email?.type === "required" && <p className="error">Please provide your email address.</p>}
+      {errors?.email?.type === "pattern" && <p className="error">Please provide a valid email address.</p>}
 
       <label>Organisation ID</label>
       <input
@@ -46,22 +46,22 @@ export default function Home() {
           maxLength: 3,
         })}
       />
-      {errors?.org && <p>You Organisation ID is 3 characters</p>}
+      {errors?.org && <p className="error">You Organisation ID is 3 characters</p>}
 
       <label>Comments</label>
-      <textarea rows="5" {...register("comments")} />
-      {errors.age && <p>You Must be older then 18 and younger then 99 years old</p>}
+      <textarea rows="3" {...register("comments")} />
+      {errors.age && <p className="error">You Must be older then 18 and younger then 99 years old</p>}
 
       <label>Reference number</label>
       <input {...register("ref", { minLength: 4, max: 4 })} />
-      {errors.ref && <p>You Organisation ID is 3 characters</p>}
+      {errors.ref && <p className="error">You Organisation ID is 3 characters</p>}
 
       <label>Add file</label>
       <input type="file" {...register("file")} />
 
       <label>Agree to the terms and conditions</label>
       <input type="checkbox" {...register("terms", { required: true })} />
-      {errors?.terms && <p>Please agree to the terms and conditions</p>}
+      {errors?.terms && <p className="error">Please agree to the terms and conditions</p>}
 
       <input type="submit" />
     </form>
